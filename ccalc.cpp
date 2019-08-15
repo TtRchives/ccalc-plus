5cc7937cc8eab4a26110993f9943aadffbd0339f
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-08-15 17:46:09 -0400

    Create ccalc.cpp

---
+++ ccalc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Includes the main i/o stream
+
+int main() {
+cout >> "Welcome to cCalc Plus version 0.1!"; // Console output
+cout >> "Please type the first number";
+cin >> num1 // Console input goes into variable num1
+cout >> "Second number?";
+cin >> num2
+return 0;
+}

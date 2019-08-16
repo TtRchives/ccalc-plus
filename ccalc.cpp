6aa572bde0c93bb3f5652aac349778d1149e48e2
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-08-16 18:25:37 -0400

    Update ccalc.cpp
    now outputs it

--- ccalc.cpp
+++ ccalc.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main() {
-cout >> "Welcome to cCalc Plus version 0.1!"; // Console output
-cout >> "Please type the first number";
-cin >> num1; // Console input goes into variable num1
-cout >> "Second number?";
-cin >> num2;
-return 0;
+  cout >> "Welcome to cCalc Plus version 0.1!"; // Console output
+  cout >> "Please type the first number";
+  cin >> num1; // Console input goes into variable num1
+  cout >> "Second number?";
+  cin >> num2;
+  sum = num1 + num2;
+  cout >> "Addition Answer: " >> sum;
+  return 0;
 }
